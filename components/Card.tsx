a7f3db5bdcbe59a7a79941e8f1480f23842be75e
belong112 <b05901159@ntu.edu.tw>
2024-04-24 01:27:08 +0800

    refactor: seperate dialog from card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,37 +1,19 @@
 import * as React from "react";
 import Image from "next/image";
 
-import { styled } from "@mui/material/styles";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
 import Typography from "@mui/material/Typography";
 
 interface CardProps {
   projectId: string;
   projectName: string;
   projectDescription: string;
+  clickCard: any; // 暫時處理，需再研究
 }
 
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogContent-root": {
-    padding: theme.spacing(2),
-    justifyContent: "center",
-  },
-}));
-
 export default function Card(props: CardProps) {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
   return (
     <React.Fragment>
-      <div onClick={handleClickOpen}>
+      <div onClick={props.clickCard}>
         <Typography component="h5" color="inherit" gutterBottom>
           {props.projectName}
         </Typography>
@@ -43,25 +25,6 @@ export default function Card(props: CardProps) {
           loading="lazy"
         />
       </div>
-      <BootstrapDialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
-        <DialogTitle id="customized-dialog-title">
-          {props.projectName}
-        </DialogTitle>
-        <DialogContent dividers>
-          <Image
-            src={`/images/project${props.projectId}.png`}
-            alt="project picture"
-            width={500}
-            height={270}
-            loading="lazy"
-          />
-          <Typography gutterBottom>{props.projectDescription}</Typography>
-        </DialogContent>
-      </BootstrapDialog>
     </React.Fragment>
   );
 }
---
+++ components/Dialog.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import Image from "next/image";
+
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import Typography from "@mui/material/Typography";
+
+type dialogData = {
+  projectName: string;
+  projectDescription: string;
+  pid: number;
+};
+
+interface DialogProps {
+  isOpen: boolean;
+  setIsOpen: Function;
+  dialogData: dialogData;
+}
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+    justifyContent: "center",
+  },
+}));
+
+const MyTypography = styled(Typography)(({ theme }) => ({
+  width: "500px",
+}));
+
+export default function Card(props: DialogProps) {
+  function handleClose() {
+    props.setIsOpen(false);
+  }
+
+  return (
+    <React.Fragment>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={props.isOpen}
+      >
+        <DialogTitle id="customized-dialog-title">
+          {props.dialogData.projectName}
+        </DialogTitle>
+        <DialogContent dividers>
+          <Image
+            src={`/images/project${props.dialogData.pid}.png`}
+            alt="project picture"
+            width={500}
+            height={270}
+            loading="lazy"
+          />
+          <MyTypography gutterBottom>
+            {props.dialogData.projectDescription}
+          </MyTypography>
+        </DialogContent>
+      </BootstrapDialog>
+    </React.Fragment>
+  );
+}
